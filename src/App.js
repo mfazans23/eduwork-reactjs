1069f2baa8ec34e55b414b31c3c5831362a75e03
mfazans23 <mfaza16717@mail.ugm.ac.id>
2022-12-30 01:07:34 +0700

    remove unnecesary files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import About from './components/About'
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Eduwork Reactjs</p>
-        <p>Muhammad Faza</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container
+      className='d-flex justify-content-center align-items-center'
+      style={{ height: '100vh' }}
+    >
+      <About
+        name='Faza'
+        city='Jepara'
+        hobbies={['sleep', 'study', 'watch footbal and movies']}
+      />
+    </Container>
   )
 }
 
